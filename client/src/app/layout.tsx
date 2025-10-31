eff543c714b2ba08633e4ac0a30a440b45b242df
x0beR-143n <pkjoker69@gmail.com>
2025-10-31 23:03:11 +0700

    Create Next.js for frontend

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Montserrat  } from "next/font/google";
+import "../style/globals.css";
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '800', '700', '900'],
+  variable: '--font-montserrat',
+});
+
+export const metadata: Metadata = {
+  title: "ClairFix",
+  description: "Image Sharing Cloud Platform",
+  icons: {
+    icon: "/camera1.png",
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${montserrat.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ client/src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit ea totam facere iste minima temporibus quaerat magni maiores in alias quam perferendis nulla enim id sapiente molestias, nemo eum eligendi!
+    </div>
+  );
+}
