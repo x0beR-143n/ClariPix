68dae7a79cec112113d974c2e40d6a444fc9c6e1
BananaOnTheTree <124771692+BananaOnTheTree@users.noreply.github.com>
2025-11-03 13:24:00 +0700

    Add SSL to connect to AWS Postgres

--- server/database/db.js
+++ server/database/db.js
@@ -10,7 +10,13 @@ const sequelize = new Sequelize(
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         dialect: 'postgres',
-        logging: console.log
+        logging: console.log,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
     }
 );
 
